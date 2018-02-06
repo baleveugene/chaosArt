3d6175c33666950958046a59b7aaa3c72db06bb8
Eugene Balev <balev.eugene@gmail.com>
2018-02-06 19:18:33 +0300

    Add files via upload

---
+++ domain/Comment.java
@@ -0,0 +1,34 @@
+package by.java.dokwork.domain;
+
+public class Comment {
+
+	private String id;
+	private String userId;
+	private String artId;
+	private String text;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getArtId() {
+		return artId;
+	}
+	public void setArtId(String artId) {
+		this.artId = artId;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+}
